fa309f3d407bfe511f20c82c30eb14e864d6e28f
BeokBeok <kekemusa37@gmail.com>
2022-11-14 17:51:35 +0900

    7. Remember in Composition

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -16,16 +16,11 @@ import androidx.compose.ui.unit.dp
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
-            Text(text = "You've had $count glasses.")
+            Text("You've had $count glasses.")
         }
-        Button(
-            modifier = Modifier.padding(top = 8.dp),
-            onClick = { count++ },
-            enabled = count < 10
-        ) {
-            Text(text = "Add one")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
