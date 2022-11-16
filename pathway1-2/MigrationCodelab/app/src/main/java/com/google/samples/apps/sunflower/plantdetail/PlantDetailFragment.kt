aabafddc9230af3abee053439994b2d56099aa2f
BeokBeok <kekemusa37@gmail.com>
2022-11-16 18:09:09 +0900

    10. ViewCompositionStrategy

--- pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ pathway1-2/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -23,6 +23,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -108,9 +109,12 @@ class PlantDetailFragment : Fragment() {
                     else -> false
                 }
             }
-            composeView.setContent {
-                MaterialTheme {
-                    PlantDetailDescription()
+            composeView.apply {
+                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+                setContent {
+                    MaterialTheme {
+                        PlantDetailDescription(plantDetailViewModel = plantDetailViewModel)
+                    }
                 }
             }
         }
