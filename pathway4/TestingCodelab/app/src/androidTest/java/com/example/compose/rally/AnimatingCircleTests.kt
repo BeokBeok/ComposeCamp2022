0d0bcdfe59247724e14675fce07a43587f223ad8
BeokBeok <kekemusa37@gmail.com>
2022-12-05 19:50:14 +0900

    5. Merged and unmerged Semantics trees

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/AnimatingCircleTests.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/AnimatingCircleTests.kt
@@ -25,6 +25,9 @@ import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.MainTestClock
 import androidx.compose.ui.test.assertIsSelectable
 import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
@@ -93,7 +96,11 @@ class AnimatingCircleTests {
         }
 
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(hasContentDescription(RallyScreen.Accounts.name)),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 
